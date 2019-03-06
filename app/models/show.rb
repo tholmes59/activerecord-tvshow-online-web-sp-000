b01d2d70a7f632cd166b835804f62ef304b39eb9
Tom Holmes <tholmes59@gmail.com>
2019-03-06 21:24:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,27 +5,27 @@ class Show < ActiveRecord::Base
   end 
   
   def Show::most_popular_show
-    Show.highest_rating
+    Show.order(rating: :desc).first
   end
-  
+
   def Show::lowest_rating
-    
+    Show.minimum("rating")
   end
-  
+
   def Show::least_popular_show
-    
+    Show.order(rating: :asc).first
   end
-  
+
   def Show::ratings_sum
-    
+    Show.sum("rating")
   end
-  
+
   def Show::popular_shows
-    
+    Show.where("rating > 5")
   end
-  
+
   def Show::shows_by_alphabetical_order
-    
+    Show.order(name: :asc)
   end
   
 end
